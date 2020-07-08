d9371603db1ebe50f81d3a93ac34351b0d9c7d51
Tiago Bento <tiagobentofernandes@gmail.com>
2020-07-08 17:37:28 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Re-trigger Workflows on open PRs
+
+GitHub Action to re-trigger Workflows on open PRs when a commit happens on this PRs base branch.
+
+### Motivation
+
+When working on repositories where many PRs tend to be open at the same time, it's often difficult to keep track of which checks should be re-run or not. When a commit happens on a PR's target branch, it's a good practice to re-run the checks to prevent Semantic Conflicts from happening.
+
+This GitHub Action simply identifies the open PRs targeting the branch where the action is running and pushes an empty commit to the PRs branches, triggering the necessary Workflows again.
+
+**NOTE:** The provided GitHub Token must be a Maintainer of the repository, otherwise it won't be able to push to PR branches. Also, the PRs to be updated must have "Allow edits from Maintainers" checked.
+
+### Important notes
+
+Everything comes with a cost. This action is not different, so there are some drawbacks:
+1. An empty commit is pushed to PR branches to re-trigger Workflows. ([GitHub's API doesn't allow re-running successful Workflow runs.](https://github.community/t/cannot-re-run-a-successful-workflow-run-using-the-rest-api/123661))
+2. All the PRs will be updated at once, making GitHub schedule many jobs to run after an update is made to the target branch.
+
+Please be aware of those topics before using this GitHub Action! It might be very useful, but it can be that it's not ideal for your use-case.
+
+### Usage
+
+```yaml
+name: "Re-trigger Workflows on open PRs"
+
+on:
+  push:
+    # Here you can limit the branches where you want this action to be run
+  
+jobs:
+  retrigger-workflows-on-open-prs:
+    runs-on: ubuntu-latest
+
+    steps:
+      - uses: tiagobento/retrigger-workflow-on-open-prs@master
+        with:
+          github_token: ${{ secrets.MY_TOKEN_WITH_MAINTAINER_PERMISSIONS }}
+```
---
+++ index.js
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const core = require("@actions/core");
+const github = require("@actions/github");
+const {Octokit} = require("@octokit/rest")
+const fetch = require("node-fetch");
+
+
+function getRef(octokit, data) {
+    return octokit.git.getRef({
+        owner: data.owner,
+        repo: data.repo,
+        ref: data.ref
+    }).then(res => res.data.object.sha);
+}
+
+function getCommitTree(octokit, data, sha) {
+    return octokit.repos.getCommit({
+        owner: data.owner,
+        repo: data.repo,
+        ref: sha
+    }).then(res => ({sha: res.data.commit.tree.sha, commitSha: sha}));
+}
+
+function createEmptyCommit(octokit, data, tree) {
+    return octokit.git.createCommit({
+        owner: data.owner,
+        repo: data.repo,
+        message: data.message,
+        tree: tree.sha,
+        parents: [tree.commitSha]
+    }).then(res => res.data.sha)
+}
+
+function updateRef(octokit, data, sha) {
+    return octokit.git.updateRef({
+        owner: data.owner,
+        repo: data.repo,
+        ref: data.ref,
+        sha: sha,
+        force: false
+    }).then(res => res.data);
+}
+
+function createEmptyCommitOnGitHub(opts) {
+
+    if (!opts || !opts.owner || !opts.repo || !opts.ref || !opts.message || !opts.token) {
+        return Promise.reject(new Error('Invalid parameters'))
+    }
+
+    const data = {
+        owner: opts.owner,
+        repo: opts.repo,
+        ref: opts.ref || 'heads/main',
+        message: opts.message
+    }
+
+    const octokit = new Octokit({auth: opts.token});
+
+    return getRef(octokit, data)
+        .then(sha => getCommitTree(octokit, data, sha))
+        .then(tree => createEmptyCommit(octokit, data, tree))
+        .then(sha => updateRef(octokit, data, sha));
+}
+
+async function run() {
+
+    const githubToken = core.getInput("github_token");
+
+    const owner = github.context.repo.owner;
+    const repo = github.context.repo.repo;
+    const branch = github.context.ref.split("/").pop();
+
+    const githubApiDomain = `https://api.github.com`;
+    const authHeaders = {
+        headers: {
+            Authorization: "token " + githubToken,
+            Accept: "application/vnd.github.v3+json"
+        }
+    };
+
+    const openPrs = await fetch(`${githubApiDomain}/repos/${owner}/${repo}/pulls?state=open&base=${branch}`, authHeaders)
+        .then(c => c.json())
+        .then(prs => prs.filter(pr => !pr.user.login.includes("dependabot")));
+
+    console.log(`Found ${openPrs.length} open PRs targeting '${branch}'`);
+
+    const newCommits = openPrs.map(pr => {
+        console.log(`Re-triggering workflows on #${pr.number}: ${pr.title}`);
+        return createEmptyCommitOnGitHub({
+            owner: pr.user.login,
+            repo: repo,
+            ref: `heads/${pr.head.ref}`,
+            token: githubToken,
+            message: `New commit on '${branch}'. Re-triggering workflows 🚀`,
+        }).then(res => {
+            console.log(`Created ${res.object.sha} on #${pr.number}: ${pr.title}`)
+            return res.object.sha;
+        });
+    })
+
+    return Promise.all(newCommits);
+}
+
+run()
+    .then(() => console.log("Finished."))
+    .catch(e => core.setFailed(e.message));
