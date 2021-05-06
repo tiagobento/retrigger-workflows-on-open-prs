e3a8e049e8da67551ef1c43c7b29734352bf6101
Justin Vanderhooft <justinvdhooft@gmail.com>
2021-05-06 13:57:20 +0000

    feat: use API rather than empty commit

--- index.js
+++ index.js
@@ -16,53 +16,22 @@
 
 const core = require("@actions/core");
 const github = require("@actions/github");
-const {Octokit} = require("@octokit/rest")
+const { Octokit } = require("@octokit/rest")
 const fetch = require("node-fetch");
 
-
-function getRef(octokit, data) {
-    console.info(`Getting ref for ${data.owner}/${data.repo}#${data.ref}`);
-    return octokit.git.getRef({
-        owner: data.owner,
-        repo: data.repo,
-        ref: data.ref
-    }).then(res => res.data.object.sha);
-}
-
-function getCommitTree(octokit, data, sha) {
-    console.info(`Getting commit tree for ${data.owner}/${data.repo}#${sha}`);
-    return octokit.repos.getCommit({
-        owner: data.owner,
-        repo: data.repo,
-        ref: sha
-    }).then(res => ({sha: res.data.commit.tree.sha, commitSha: sha}));
-}
-
-function createEmptyCommit(octokit, data, tree) {
-    console.info(`Creating empty commit... ${data.owner}/${data.repo}#${tree.sha}->${tree.commitSha}`);
-    return octokit.git.createCommit({
-        owner: data.owner,
-        repo: data.repo,
-        message: data.message,
-        tree: tree.sha,
-        parents: [tree.commitSha]
-    }).then(res => res.data.sha)
-}
-
-function updateRef(octokit, data, sha) {
-    console.info(`Updating ref... ${data.owner}/${data.repo}#${data.ref}-${sha}`);
-    return octokit.git.updateRef({
+async function dispatchWorkflowEvent(octokit, data) {
+    console.info(`Dispatching "workflow_dispatch"... ${data.owner}/${data.repo}/${data.ref}`);
+    return octokit.git.actions.createWorkflowDispatch({
         owner: data.owner,
         repo: data.repo,
         ref: data.ref,
-        sha: sha,
-        force: false
-    }).then(res => res.data);
+        workflow_id: data.workflow_id
+    }).then(res => res.data.sha)
 }
 
-function createEmptyCommitOnGitHub(opts) {
+async function dispatchWorkflowEventToGithub(opts) {
 
-    if (!opts || !opts.owner || !opts.repo || !opts.ref || !opts.message || !opts.token) {
+    if (!opts || !opts.owner || !opts.repo || !opts.ref || !opts.message || !opts.token || !opts.workflow_id) {
         return Promise.reject(new Error('Invalid parameters'))
     }
 
@@ -75,15 +44,13 @@ function createEmptyCommitOnGitHub(opts) {
 
     const octokit = new Octokit({auth: opts.token});
 
-    return getRef(octokit, data)
-        .then(sha => getCommitTree(octokit, data, sha))
-        .then(tree => createEmptyCommit(octokit, data, tree))
-        .then(sha => updateRef(octokit, data, sha));
+    return dispatchWorkflowEvent(octokit, data, tree);
 }
 
 async function run() {
 
     const githubToken = core.getInput("github_token");
+    const workflowId = core.getInput("workflow_id");
 
     const owner = github.context.repo.owner;
     const repo = github.context.repo.repo;
@@ -103,21 +70,21 @@ async function run() {
 
     console.log(`Found ${openPrs.length} open PR(s) targeting '${branch}'`);
 
-    const newCommits = openPrs.map(pr => {
+    const dispatches = openPrs.map(pr => {
         console.log(`Re-triggering workflows on #${pr.number}: ${pr.title}`);
-        return createEmptyCommitOnGitHub({
+        return dispatchWorkflowEventToGithub({
             owner: pr.head.user.login,
             repo: repo,
             ref: `heads/${pr.head.ref}`,
             token: githubToken,
-            message: `New commit on '${branch}'. Re-triggering workflows 🚀`,
+            workflow_id: workflowId 
         }).then(res => {
-            console.log(`Created ${res.object.sha} on #${pr.number}: ${pr.title}`)
+            console.log(`Dispatched workflowId on #${pr.number}: ${pr.title}`)
             return res.object.sha;
         });
     })
 
-    return Promise.all(newCommits);
+    return Promise.all(dispatches);
 }
 
 run()
